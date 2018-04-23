01eab2812d0b381cd802dbbeb41f823239fccc4c
Wes Young <wes@barely3am.com>
2018-04-23 16:21:10 -0400

    fixes

--- csirtg_ipsml/geo.py
+++ csirtg_ipsml/geo.py
@@ -4,24 +4,21 @@ import pygeoip
 DB_SEARCH_PATHS = [
     './',
     '/usr/share/GeoIP',
-    '/usr/local/share/GeoIP'
+    '/usr/local/share/GeoIP',
 ]
 
-DB_FILE = 'GeoLite2-City.mmdb'
-
-ASN_DB_PATH = 'GeoIPASNum.dat'
-ASN_DB_PATH2 = 'GeoLiteASNum.dat'
-CITY_DB_PATH = 'GeoLiteCity.dat'
+DB_PATH = 'GeoLite2-City.mmdb'
+ASN_DB_PATH = 'GeoLite2-ASN.mmdb'
 
 asndb = None
 citydb = None
 
 for p in DB_SEARCH_PATHS:
-    if os.path.isfile(os.path.join(p, CITY_DB_PATH)):
-        citydb = pygeoip.GeoIP(os.path.join(p, CITY_DB_PATH), pygeoip.MMAP_CACHE)
+    if os.path.isfile(os.path.join(p, DB_PATH)):
+        citydb = pygeoip.GeoIP(os.path.join(p, DB_PATH), pygeoip.MMAP_CACHE)
         break
 
 for p in DB_SEARCH_PATHS:
-    if os.path.isfile(os.path.join(p, ASN_DB_PATH2)):
-        asndb = pygeoip.GeoIP(os.path.join(p, ASN_DB_PATH2), pygeoip.MMAP_CACHE)
+    if os.path.isfile(os.path.join(p, ASN_DB_PATH)):
+        asndb = pygeoip.GeoIP(os.path.join(p, ASN_DB_PATH), pygeoip.MMAP_CACHE)
         break
